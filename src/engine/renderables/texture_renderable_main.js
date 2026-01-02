cdce5fb8b40d4d70039bf18f68a0ca9fc8cdac43
BadStory Games <solomonkey02@gmail.com>
2026-01-02 11:41:01 +0300

    TextureRenderable
    Добавлена информация о текстуре и расширена функция установки текстуры

--- src/engine/renderables/texture_renderable_main.js
+++ src/engine/renderables/texture_renderable_main.js
@@ -8,11 +8,22 @@ import Renderable from "./renderable.js";
 
 
 class TextureRenderable extends Renderable {
-    constructor(tex) {
+    constructor(texturePath) {
         super();
+        super.setColor([1, 1, 1, 0]);
         super._setShader(shaders.getTextureShader());
 
-        this.texture = tex;
+        this.texture = null;
+        // Эти две переменные нужны для хранения информации о текстуре для попиксельных столкновений
+        this.textureInfo = null;
+        this.colorArray = null;
+        // Эти переменные нужны для субкласса для переопределения
+        this.elementWidth = 0;
+        this.elementHeight = 0;
+        this.elementLeftID = 0;
+        this.elementBottomID = 0;
+
+        this.setTexture(texturePath);
     }
 
 
@@ -24,7 +35,20 @@ class TextureRenderable extends Renderable {
 
     getTexture() { return this.texture; }
 
-    setTexture(tex) { this.texture = tex; }
+    setTexture(texturePath) {
+        this.texture = texturePath;
+
+        // Эти две переменные нужны для хранения информации о текстуре для попиксельных столкновений
+        this.textureInfo = texture.get(texturePath);
+        this.colorArray = null;
+
+        // Определен для одного элемента sprite для переопределения подклассом
+        // Для texture_renderable один элемент спрайта - это вся текстура целиком
+        this.elementWidth = this.textureInfo.width;
+        this.elementHeight = this.textureInfo.height;
+        this.elementLeftID = 0;
+        this.elementBottomID = 0;
+    }
 }
 
 export default TextureRenderable;

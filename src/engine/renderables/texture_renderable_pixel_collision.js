5c9872197ac19cb7e00fd5528665786a8ba26d77
BadStory Games <solomonkey02@gmail.com>
2026-01-02 11:48:33 +0300

    TextureRenderable.setColorArray()

--- src/engine/renderables/texture_renderable_pixel_collision.js
+++ src/engine/renderables/texture_renderable_pixel_collision.js
@@ -1,6 +1,15 @@
 "use strict";
 
+import * as texture from "../resources/texture.js";
 import TextureRenderable from "./texture_renderable_main.js";
+
+
+
+TextureRenderable.prototype.setColorArray = function() {
+    if (this.colorArray === null) {
+        this.colorArray = texture.getColorArray(this.texture);
+    }
+}
 
 
 

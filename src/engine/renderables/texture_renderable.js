f4eb972f5d0e3d5f877824b0e7840a910f7eb015
BadStory Games <solomonkey02@gmail.com>
2026-01-02 11:27:14 +0300

    Рефакторинг TextureRenderable

--- src/engine/renderables/texture_renderable.js
+++ src/engine/renderables/texture_renderable.js
@@ -1,30 +1,5 @@
 "use strict";
 
-import * as texture from "../resources/texture.js";
-import * as shaders from "../core/shaders.js";
-
-import Renderable from "./renderable.js";
-
-
-
-class TextureRenderable extends Renderable {
-    constructor(tex) {
-        super();
-        super._setShader(shaders.getTextureShader());
-
-        this.texture = tex;
-    }
-
-
-
-    draw(camera) {
-        texture.activate(this.texture);
-        super.draw(camera);
-    }
-
-    getTexture() { return this.texture; }
-
-    setTexture(tex) { this.texture = tex; }
-}
+import TextureRenderable from "./texture_renderable_pixel_collision.js";
 
 export default TextureRenderable;
---
+++ src/engine/renderables/texture_renderable_pixel_collision.js
@@ -0,0 +1,7 @@
+"use strict";
+
+import TextureRenderable from "./texture_renderable_main.js";
+
+
+
+export default TextureRenderable;
